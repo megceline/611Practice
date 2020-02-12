9ba07887b36a3f4800449d8de10d153d8de3c23f
meg hernandez <meghernandez@megs-MacBook-Pro.local>
2020-02-11 20:04:22 -0800

    all problems solved

--- practice.R
+++ practice.R
@@ -1,24 +1,37 @@
 ## Practice the following problem 
 
+
+y <- data.frame(a=1:5, b= c('l', 'm', 'n', 'o', 'p'), s=c(2,3,6,7,8))
+
+
+
 # What is the sum of the first 1000 positive integers?
-items<-seq(1, 1000)
+items<-1:1000
 sum(items)
-#1. Use the function c to create a vector with the average high temperatures in January for
+sum(1:1000)
+
+#Use the function c to create a vector with the average high temperatures in January for
 #Beijing, Lagos, Paris, Rio de Janeiro, San Juan, and Toronto, which are 35, 88, 42, 84, 81, 
 #and 30 degrees Fahrenheit. Call the object temp.
 c(35, 88, 42,84, 81, 30)
 temp <- c(35, 88, 42,84, 81, 30)
 print(temp)
+
 # Now create a vector with the city names and call the object city.
 city <- c("Beijing", "Lagos", "Paris", "Rio de Janeiro", "San Juan", "Toronto")
 print(city)
 
 # Use the names function and the objects defined in the previous exercises to associate the temperature data with its corresponding city.
+city_temp <- data.frame(city, temp)
+
 
 # Use the [ and : operators to access the temperature of the first three cities on the list.
-             
+# $ only
+city_temp$temp[1:3]
+
 # Use the [ operator to access the temperature of Paris and San Juan.
-                          
+city_temp[c(3,5),]
+            
 # Use the : operator to create a sequence of numbers  12,13,14,.....73
 12:73
                           
@@ -26,23 +39,26 @@ print(city)
 seq(1, 100, 2)
                           
 # Create a vector of numbers that starts at 6, does not pass 55, and adds numbers in increments of 4/7: 6, 6 + 4/7, 6 + 8/7, and so on. How many numbers does the list have? Hint: use seq and length.
-                          
+seq(6, 55, 4/7)            
+exercise_example <- seq(6, 55, 4/7)  
+length(exercise_example)
+
 # What is the class of the following object a <- seq(1, 10, 0.5)?
 a <- seq(1, 10, 0.5)
 class(a) 
-a
+
                             
 #What is the class of the following object a <- seq(1, 10)?
 a <- seq(1, 10)
 class(a)
-a
+
 
 # The class of class(a<-1) is numeric, not integer. R defaults to numeric and to force an integer, you need to add the letter L. Confirm that the class of 1L is integer.
-                          
+class(a<-1L)
 
 # Define the following vector:x <- c("1", "3", "5") and coerce it to get integers.
 x <- c("1", "3", "5")
-x
+class(x)
 as.integer(x)
 
 #  In the data frame you made in the second problem convert the temperature from Fahrenheit to Celsius. The conversion is  
@@ -50,6 +66,5 @@ as.integer(x)
 
 C <- 5/9 * (temp-32)
 C
-
 
 
